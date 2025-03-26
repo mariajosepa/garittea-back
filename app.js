c8da0bbbe9e6c66cd22015bd7c1a14727672a4b6
mariajose <mariamariapava123@gmail.com>
2025-03-26 08:01:10 -0500

    [NEW] Create base route and installed nodemon

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 
 //define the routes
 app.use('/person', personRoutes);
+app.get('/', (req, res) => {
+  res.send('Welcome to the root route!');
+});
+
 
 //launch the server
 app.listen(PORT, () => {
