5dca0ce8f69828dac8cc46b001c560927527d2f9
mariajose <mariamariapava123@gmail.com>
2025-03-30 10:16:18 -0500

    [MOD] Deleted field from person schema

---
+++ prisma/migrations/20250330145953_phone_to_string/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "faculty" ALTER COLUMN "phone" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "person" ALTER COLUMN "cellphone" SET DATA TYPE TEXT;
---
+++ prisma/migrations/20250330150905_remove_field_person/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `canmanage` on the `person` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "person" DROP COLUMN "canmanage";
