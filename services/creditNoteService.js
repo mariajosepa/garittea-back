7e1e1b4240814ad903f72eb41ed540d8fd948eeb
mariajose <mariamariapava123@gmail.com>
2025-04-27 17:21:22 -0500

    new: added credit note create method

--- services/creditNoteService.js
+++ services/creditNoteService.js
@@ -3,9 +3,17 @@ import prisma from '../prisma/client.js';
 export const getAllCreditNotes = async () => {
     return await prisma.creditNote.findMany({
         include: {
-            
             initialBill: true,
             finishBill: true
         }
     });
+}
+export const createNoteCredit = async ({idcreditNote, idInicialBill, idFinishBill}) => {
+  return await prisma.creditNote.create({
+    data : {
+      idcreditNote,
+      idInicialBill,
+      ...(idFinishBill !== undefined && { idFinishBill })
+    }
+  })
 }
